acad3dcec4daf0e71b27024cd2292f142a487735
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2022-12-22 00:44:20 +0700

    Config Theme

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,49 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import Link from "next/link";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>LeetCode Practitioner</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/EpicHigh/leetcode-practitioner",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/EpicHigh/leetcode-practitioner",
   footer: {
-    text: 'Nextra Docs Template',
+    text: (
+      <span>
+        MIT {new Date().getFullYear()} © Thank you for visiting our JavaScript
+        LeetCode practices documentation website.
+        <br />
+        I hope that you have found this resource helpful in improving your
+        coding skills and preparing for technical interviews.
+        <br />
+        Made with ❤️ by{" "}
+        <Link
+          className="footer-link"
+          href="https://linkedin.com/in/prasit-tongpradit"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Prasit Tongpradit
+        </Link>{" "}
+        and I would like to thank{" "}
+        <Link
+          className="footer-link"
+          href="https://www.youtube.com/@NeetCode"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          NeetCode
+        </Link>{" "}
+        for their video resources. Keep practicing and happy coding!
+      </span>
+    ),
   },
-}
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s – LeetCode Practitioner",
+    };
+  },
+};
 
-export default config
+export default config;
