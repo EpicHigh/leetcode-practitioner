393d3f45b397a0088818827db0ab82dfc430ec39
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-01-11 10:38:58 +0700

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import { AppProps } from "next/app";
 import "../styles/global.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const MyApp = (props: AppProps) => {
-  const { Component, pageProps } = props;
+	const { Component, pageProps } = props;
 
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Component {...pageProps} />
+			<Analytics />
+		</>
+	);
 };
 
 export default MyApp;
