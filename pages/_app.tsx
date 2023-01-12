9532ee7cda0eb45c3660ee5a4f948f386aa1571e
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-01-12 18:57:50 +0700

    Add Roboto font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,20 @@
 import '../styles/global.css';
+import { Roboto } from '@next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 import { AppProps } from 'next/app';
+
+const roboto = Roboto({ weight: '400' });
 
 const MyApp = (props: AppProps) => {
 	const { Component, pageProps } = props;
 
 	return (
 		<>
+			<style jsx global>{`
+				html {
+					font-family: ${roboto.style.fontFamily};
+				}
+			`}</style>
 			<Component {...pageProps} />
 			<Analytics />
 		</>
