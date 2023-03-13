3eae5d3cf696cbc44da052ca0f0fc48ff635024b
Prasit Tongpradit <prasit_tongpradit@windowslive.com>
2023-03-14 02:53:24 +0700

    Enable pre and post scripts and add next-sitemap package for generating sitemap in the postbuild script

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+	siteUrl: 'https://leetcode-practitioner.vercel.app/',
+	generateRobotsTxt: true, // (optional)
+	generateIndexSitemap: false,
+};
